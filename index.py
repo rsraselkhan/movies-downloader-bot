125a20924276fbde2450d71a53c6be24965b0f01
rsraselkhan <164967114+rsraselkhan@users.noreply.github.com>
2024-04-21 12:12:49 +0600

    Update index.py

--- index.py
+++ index.py
@@ -14,13 +14,13 @@ bot = Bot(TOKEN)
 
 
 def welcome(update, context) -> None:
-    update.message.reply_text(f"Hello {update.message.from_user.first_name}, Welcome to SB Movies.\n"
+    update.message.reply_text(f"Hello {update.message.from_user.first_name}, Welcome to The Movie World.\n"
                               f"🔥 Download Your Favourite Movies For 💯 Free And 🍿 Enjoy it.")
     update.message.reply_text("👇 Enter Movie Name 👇")
 
 
 def find_movie(update, context):
-    search_results = update.message.reply_text("Processing...")
+    search_results = update.message.reply_text("Searching...")
     query = update.message.text
     movies_list = search_movies(query)
     if movies_list:
